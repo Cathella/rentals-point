38982759bfdc4a7a1fdd38457980e975adc240ec
Cathella <ncathie214@gmail.com>
2023-06-12 17:41:09 +0300

    Made recents responsive

--- src/components/Recents.tsx
+++ src/components/Recents.tsx
@@ -6,10 +6,10 @@ function Recents() {
       <div className="container">
         <h3 className="text-center pb-5">Recently Added Properties</h3>
         <div className="row">
-          <div className="col-lg-3">
-            <div className="bg-white p-3 border-3 rounded-5">
+          <div className="col-md-6 col-lg-3">
+            <div className="house-entry bg-white p-3 border-3 rounded-5">
               <div className="house-one"></div>
-              <div className="d-flex">
+              <div className="price">
                 <h4>UGX. 750,000</h4>
                 <span className="times">(per month)</span>
               </div>
@@ -19,10 +19,10 @@ function Recents() {
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="bg-white p-3 border-3 rounded-5">
+          <div className="col-md-6 col-lg-3">
+            <div className="house-entry bg-white p-3 border-3 rounded-5">
               <div className="house-two"></div>
-              <div className="d-flex">
+              <div className="price">
                 <h4>UGX. 500,000</h4>
                 <span className="times">(per month)</span>
               </div>
@@ -32,10 +32,10 @@ function Recents() {
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="bg-white p-3 border-3 rounded-5">
+          <div className="col-md-6 col-lg-3">
+            <div className="house-entry bg-white p-3 border-3 rounded-5">
               <div className="house-three"></div>
-              <div className="d-flex">
+              <div className="price">
                 <h4>UGX. 370,000</h4>
                 <span className="times">(per month)</span>
               </div>
@@ -45,10 +45,10 @@ function Recents() {
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="bg-white p-3 border-3 rounded-5">
+          <div className="col-md-6 col-lg-3">
+            <div className="house-entry bg-white p-3 border-3 rounded-5">
               <div className="house-four"></div>
-              <div className="d-flex">
+              <div className="price">
                 <h4>UGX. 900,000</h4>
                 <span className="times">(per month)</span>
               </div>
