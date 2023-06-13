25cf3c1979c874a460b1f63bad8d4b59edc60eed
Cathella <ncathie214@gmail.com>
2023-06-14 00:38:06 +0300

    Fix mobile menu toggle

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,35 +7,41 @@ function Nav() {
         <a className="navbar-brand" href="#">
           <img src={TopLogo} alt="RentalsPoint" />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Rent</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Sell</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Buy</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">For Landlords</a>
-            </li>
-          </ul>
-          <ul className="navbar-nav mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#" id="greened">Login</a>
-            </li>
-            <li className="nav-item">
-              <a className="px-3 py-2 custom-button custom-dark-green" href="#">Sign Up</a>
-            </li>
-          </ul>
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">RentalsPoint</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body"> 
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Rent</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sell</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Buy</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">For Landlords</a>
+              </li>
+            </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#" id="greened">Login</a>
+              </li>
+              <li className="nav-item">
+                <a className="px-3 py-2 custom-button custom-dark-green" href="#">Sign Up</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
