39537a64ceae57a8ba2e68a6a0b3efe1097084f6
Cathella <ncathie214@gmail.com>
2023-06-16 10:22:06 +0300

    Load property, categories data from object

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,15 +1,21 @@
-function Category() {
-  return (
-    <>
-      <div className="text-center col-lg-4">
-        <div className="apartments-img mb-3"></div>
-        <h4 className="mb-3">Apartments</h4>
-        <div>
-          <a href="#" className="categories-button px-3">100+ Properties</a>
-        </div>
-      </div>
-    </>
-  );
+interface CategoryProps {
+  title: string;
+  image: string;
+  total: number;
 }
 
-export default Category;
+const Category: React.FC<CategoryProps> = ({ title, total, image }) => {
+  return (
+    <div className="text-center col-lg-4">
+      <div className=" mb-3">
+        <img src={image} alt={title} className='img-fluid apartments-img' />
+      </div>
+      <h4 className="mb-3">{title}</h4>
+        <div>
+          <a href="#" className="categories-button px-3">{total} Properties</a>
+        </div>
+    </div>
+  );
+};
+
+export default Category; 
--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -1,23 +1,34 @@
 import LocationIcon from '../../public/location.svg';
 
-function Property() {
+interface PropertyProps {
+  title: string;
+  price: number;
+  location: string;
+  description: string;
+  image: string;
+  bedrooms: number;
+  baths?: number;
+  parking?: string;
+}
+
+const Property: React.FC<PropertyProps> = ({ title, price, location, image }) => {
   return (
-    <>
-      <div className="col-md-6 col-lg-3">
-        <div className="house-entry bg-white p-3 border-3 rounded-5">
-          <div className="house-one"></div>
-          <div className="price">
-            <h4>UGX. 750,000</h4>
-            <span className="times">(per month)</span>
-          </div>
-          <div className="location">
-            <img src={LocationIcon} alt="Icon" />
-            <span>Kiwatule</span>
-          </div>
+    <div className="col-md-6 col-lg-3">
+      <div className="house-entry bg-white p-3 border-3 rounded-5">
+        <div className="">
+          <img src={image} alt={title} className='img-fluid house-one' />
+        </div>
+        <div className="price">
+          <h4>UGX. {price}</h4>
+          <span className="times">(per month)</span>    
+        </div>
+        <div className="location">
+          <img src={LocationIcon} alt="Icon" />
+          <span>{location}</span>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Property;
