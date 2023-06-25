de47c838c9a975d395ae04d11f3f3420d3e4632f
Cathella <ncathie214@gmail.com>
2023-06-25 11:47:35 +0300

    Styled form component

--- src/PropertyForm.tsx
+++ src/PropertyForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import Nav from './components/Nav';
 
 const PropertyForm = () => {
   const [title, setTitle] = useState('');
@@ -56,147 +57,212 @@ const PropertyForm = () => {
   };
 
   return (
-    <div>
-      <h2>Add Property</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Title:
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Image URL 1:
-            <input type="text" value={imgUrl1} onChange={(e) => setImgUrl1(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Image URL 2:
-            <input type="text" value={imgUrl2} onChange={(e) => setImgUrl2(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Image URL 3:
-            <input type="text" value={imgUrl3} onChange={(e) => setImgUrl3(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Image URL 4:
-            <input type="text" value={imgUrl4} onChange={(e) => setImgUrl4(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Image URL 5:
-            <input type="text" value={imgUrl5} onChange={(e) => setImgUrl5(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Description:
-            <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Price:
-            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Bedrooms:
-            <input type="text" value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Baths:
-            <input type="text" value={baths} onChange={(e) => setBaths(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Kitchen:
-            <input type="checkbox" checked={kitchen} onChange={(e) => setKitchen(e.target.checked)} />
-          </label>
-          <label>
-            Store:
-            <input type="checkbox" checked={store} onChange={(e) => setStore(e.target.checked)} />
-          </label>
-          <label>
-            Water:
-            <input type="checkbox" checked={water} onChange={(e) => setWater(e.target.checked)} />
-          </label>
-          <label>
-            Electricity:
-            <input
-              type="checkbox"
-              checked={electricity}
-              onChange={(e) => setElectricity(e.target.checked)}
-            />
-          </label>
-          <label>
-            Security:
-            <input type="checkbox" checked={security} onChange={(e) => setSecurity(e.target.checked)} />
-          </label>
-        </div>
+    <>
+      <Nav />
+      <div className='py-5'>
+        <div className="container">
+          <h3 className="text-center mb-5">List your Property</h3>
+          <div className="row">
+            <div className="col-lg-6 mx-auto">
+              <form onSubmit={handleSubmit}>
+                <h5 className="green-txt mb-5 text-center">Property Information</h5>
+                <div className='form-floating mb-4'>
+                    <input 
+                      type="text"
+                      className='form-control' 
+                      placeholder=''
+                      value={title} 
+                      onChange={(e) => setTitle(e.target.value)} />
+                      <label>Property Title</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <select 
+                    value={propertyType} 
+                    className="form-select"
+                    onChange={(e) => setPropertyType(e.target.value)}>
+                    <option value="">-- Select Property Type --</option>
+                    <option value="Apartment">Apartment</option>
+                    <option value="House">House</option>
+                    <option value="Rental">Rental</option>
+                    <option value="Office">Office</option>
+                    <option value="Shop">Shop</option>
+                  </select>
+                  <label>Property Type</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <select 
+                    value={location} 
+                    className="form-select"
+                    onChange={(e) => setLocation(e.target.value)}>
+                    <option value="">-- Select Location --</option>
+                    <option value="kampala">Kampala</option>
+                    <option value="namugongo">Namugongo</option>
+                    <option value="kira">Kira</option>
+                    <option value="masaka">Masaka</option>
+                  </select>
+                  <label>Location</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="number" 
+                    className="form-control"
+                    placeholder=""
+                    value={price} 
+                    onChange={(e) => setPrice(e.target.value)} />
+                    <label>Property Price</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <select 
+                    value={paymentFreq} 
+                    className="form-select"
+                    onChange={(e) => setPaymentFreq(e.target.value)}>
+                    <option value="">-- Select Payment Frequency --</option>
+                    <option value="night">Daily</option>
+                    <option value="week">Weekly</option>
+                    <option value="month">Monthly</option>
+                    <option value="year">Yearly</option>
+                  </select>
+                  <label>Payment Frequency</label>
+                </div>
+                <div className="form-floating mb-5">
+                  <textarea 
+                    value={description} 
+                    className="form-control"
+                    placeholder=""
+                    onChange={(e) => setDescription(e.target.value)} />
+                  <label>About Property</label>
+                </div>
 
-        <div>
-          <label>
-            Parking:
-            <select value={parking} onChange={(e) => setParking(e.target.value)}>
-              <option value="">-- Select Parking --</option>
-              <option value="Available">Available</option>
-              <option value="Not Available">Not Available</option>
-            </select>
-          </label>
+                <h5 className="border-top pt-5 green-txt text-center mb-5">Property Photos</h5>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="text" 
+                    className='form-control' 
+                    placeholder=''
+                    value={imgUrl1} 
+                    onChange={(e) => setImgUrl1(e.target.value)} />
+                  <label>Image URL 1:</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="text" 
+                    className='form-control' 
+                    placeholder=''
+                    value={imgUrl2} 
+                    onChange={(e) => setImgUrl2(e.target.value)} />
+                    <label>Image URL 2</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="text"
+                    className='form-control' 
+                    placeholder='' 
+                    value={imgUrl3} 
+                    onChange={(e) => setImgUrl3(e.target.value)} />
+                    <label>Image URL 3</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="text"
+                    className='form-control' 
+                    placeholder='' 
+                    value={imgUrl4} 
+                    onChange={(e) => setImgUrl4(e.target.value)} />
+                    <label>Image URL 4</label>
+                </div>
+                <div className="form-floating mb-5">
+                  <input 
+                    type="text" 
+                    className='form-control' 
+                    placeholder=''
+                    value={imgUrl5} 
+                    onChange={(e) => setImgUrl5(e.target.value)} />
+                    <label>Image URL 5</label>
+                </div>
+
+                <h5 className="border-top pt-5 green-txt text-center mb-5">Amenities</h5>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="number" 
+                    className="form-control"
+                    placeholder=""
+                    value={bedrooms} 
+                    onChange={(e) => setBedrooms(e.target.value)} />
+                    <label>Bedrooms</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input 
+                    type="number" 
+                    className="form-control"
+                    placeholder=""
+                    value={baths} 
+                    onChange={(e) => setBaths(e.target.value)} />
+                    <label>Baths</label>
+                </div>
+                <div className="form-floating mb-5">
+                  <select 
+                    value={parking} 
+                    className="form-select" 
+                    onChange={(e) => setParking(e.target.value)}>
+                    <option value="">-- Select Parking --</option>
+                    <option value="Available">Available</option>
+                    <option value="Not Available">Not Available</option>
+                  </select>
+                  <label>Parking</label>
+                </div>
+
+                <h5 className="border-top pt-5 green-txt text-center mb-5">Other Features</h5>
+                <div className='other-amenities'>
+                  <div className="form-check mb-3">
+                    <input 
+                      type="checkbox" 
+                      className="form-check-input"
+                      checked={kitchen} 
+                      onChange={(e) => setKitchen(e.target.checked)} />
+                      <label>Kitchen</label>
+                  </div>
+                  <div className="form-check mb-3">
+                    <input 
+                      type="checkbox"
+                      className="form-check-input" 
+                      checked={store} 
+                      onChange={(e) => setStore(e.target.checked)} />
+                      <label>Store Room</label>
+                  </div>
+                  <div className="form-check mb-3">
+                    <input 
+                      type="checkbox" 
+                      className="form-check-input" 
+                      checked={water} 
+                      onChange={(e) => setWater(e.target.checked)} />
+                    <label>Water</label>
+                  </div>
+                  <div className="form-check mb-3">
+                    <input
+                      type="checkbox"
+                      checked={electricity}
+                      className="form-check-input" 
+                      onChange={(e) => setElectricity(e.target.checked)}
+                    />
+                    <label>Electricity</label>
+                  </div>
+                  <div className="form-check mb-3">
+                    <input 
+                      type="checkbox"
+                      className="form-check-input" 
+                      checked={security} 
+                      onChange={(e) => setSecurity(e.target.checked)} />
+                      <label>Security</label>
+                  </div>
+                </div>
+
+                <button id='login-submit' className="my-4 custom-button form-control custom-dark-green text-white" type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div>
-          <label>
-            Location:
-            <select value={location} onChange={(e) => setLocation(e.target.value)}>
-              <option value="">-- Select Location --</option>
-              <option value="kampala">Kampala</option>
-              <option value="namugongo">Namugongo</option>
-              <option value="kira">Kira</option>
-              <option value="masaka">Masaka</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Payment Frequency:
-            <select value={paymentFreq} onChange={(e) => setPaymentFreq(e.target.value)}>
-              <option value="">-- Select Payment Frequency --</option>
-              <option value="night">Daily</option>
-              <option value="week">Weekly</option>
-              <option value="month">Monthly</option>
-              <option value="year">Yearly</option>
-              {/* Add more options as needed */}
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Property Type:
-            <select value={propertyType} onChange={(e) => setPropertyType(e.target.value)}>
-              <option value="">-- Select Property Type --</option>
-              <option value="Apartment">Apartment</option>
-              <option value="House">House</option>
-              <option value="Rental">Rental</option>
-              <option value="Office">Office</option>
-              <option value="Shop">Shop</option>
-            </select>
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
