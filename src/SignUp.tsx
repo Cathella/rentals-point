223f2e69a4fc6caae8c3634d7be32c5bb08196f4
Cathella <ncathie214@gmail.com>
2023-07-01 12:03:06 +0300

    Clear form after signup

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,8 +1,45 @@
 import { Link } from 'react-router-dom';
 import TopLogo from './assets/logowhite.svg';
 import SideImage from './components/SideImage';
+import { useState, FormEvent } from 'react';
+import axios from 'axios';
 
-function SignUp() {
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [account, setAccount] = useState('');
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Create the user object with form data
+    const user = {
+      username,
+      email,
+      password,
+      account
+    };
+
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/users', { user });
+
+      if (response.status === 201) {
+        // User signup successful, perform any necessary actions (e.g., redirect, show success message)
+        console.log('User signup successful');
+
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setAccount('');
+      } else {
+        console.error('Failed to signup:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
   return (
     <>
     <div className="">
@@ -19,28 +56,42 @@ function SignUp() {
               <a id='with-email' href="#collapseExample" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">Signup with Email</a>
               <div className="collapse wid-100" id="collapseExample">
                 <div className="border shadow-sm px-4 py-4 rounded-4">
-                  <form>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                      <input 
+                        type="text"
+                        placeholder=''
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className='form-control' />
+                        <label>Username</label>
+                    </div>
                     <div className="form-floating mb-3">
                       <input 
                         type="email"
-                        placeholder='' 
+                        placeholder=''
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
                         className='form-control' />
-                        <label>Email Address</label>
+                        <label>Email</label>
                     </div>
                     <div className="form-floating mb-3">
                       <input 
                         type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         placeholder='' 
                         className='form-control' />
                         <label>Password</label>
                     </div>
                     <div className="form-floating mb-3">
                       <select 
-                        value='' 
+                        value={account} 
+                        onChange={(e) => setAccount(e.target.value)}
                         className="form-select">
                         <option value="">-- Select Account --</option>
-                        <option value="Tenant">I am a Tenant</option>
-                        <option value="Landlord">I am a Landlord</option>
+                        <option value="tenant">I am a Tenant</option>
+                        <option value="landlord">I am a Landlord</option>
                       </select>
                       <label>Choose Account</label>
                     </div>
